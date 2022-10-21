8d89b40b3aeaa2599d0b5ef2478987ca03230f82
kyle long <kylel95@vt.edu>
2022-10-21 02:39:11 -0700

    first commit of new portfolio

--- src/Projects.js
+++ src/Projects.js
@@ -6,7 +6,7 @@ const Projects = () => {
             <h3 className='projectSectionHeader'>Projects</h3>
             <div className="project">
                 <p className='projectTitle'>Corked Up</p>
-                <a href="https://www.corkedup.wine/" target="_blank">https://www.corkedup.wine/</a>
+                <a href="https://www.corkedup.wine/" target="_blank" rel="noreferrer">https://www.corkedup.wine/</a>
                 <p className="projectDescription">Catering to wine lovers: find events, daily flash deals on nice bottles, and recipes for pairiing. 
                     All to enhance your wine drinking experience.
                 </p>
