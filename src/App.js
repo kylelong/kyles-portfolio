813d9274374c5b25f90663acf5a213d60d75c49e
kyle long <kylel95@vt.edu>
2023-11-28 07:10:36 -0500

    update project list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import './App.css';
-import Menu from './Menu';
-import headshot from './headshot.jpeg';
-import Projects from './Projects';
+import "./App.css";
+import Menu from "./Menu";
+import headshot from "./headshot.jpeg";
+import Projects from "./Projects";
 
 function App() {
   return (
     <div className="App">
-     <Menu />
-     <div className="biocontainer">
-      <div className="bioimagecontainer">
-        <div className="bioimage">
-          <img src={headshot} alt="headshot" className="bioimagesize" />
+      <Menu />
+      <div className="biocontainer">
+        <div className="bioimagecontainer">
+          <div className="bioimage">
+            <img src={headshot} alt="headshot" className="bioimagesize" />
+          </div>
         </div>
       </div>
-     </div>
-     <p className="intro">Ciao, I'm Kyle, a software engineer living in San Francisco, CA. I'm currently searching for fullstack, frontend, and backend engineering roles.
-     Feel free to reach out, let's talk! 
-     <br /> <br /> My interests include: golf 🏌🏾, running 🏃🏾‍♂️, drinking delicious wine &#x1F377;, playing cello 🎻, and traveling 	&#x1f6f8;</p>
-    <Projects />
+      <div>
+        <p className="intro">
+          Ciao, I'm Kyle, a software engineer living in San Francisco, CA. I'm
+          currently searching for fullstack, frontend, and backend engineering
+          roles. Feel free to reach out, let's talk!
+          <br /> <br /> My interests include: golf 🏌🏾, running 🏃🏾‍♂️, drinking
+          delicious wine &#x1F377;, playing cello 🎻, and traveling &#x1f6f8;
+        </p>
+        <Projects />
+      </div>
     </div>
   );
 }
--- src/Projects.js
+++ src/Projects.js
@@ -4,7 +4,22 @@ const Projects = () => {
   return (
     <div className="projectContainer">
       <h3 className="projectSectionHeader">Projects</h3>
-       <div className="project">
+      <div className="project">
+        <p className="projectTitle">JoJo</p>
+        <div className="projectLinks">
+          <a
+            href="https://jojo.so"
+            target="_blank"
+            rel="noreferrer"
+            className="projectLink"
+          >
+            website
+          </a>
+        </div>
+
+        <p className="projectDescription">Conversational language learning</p>
+      </div>
+      <div className="project">
         <p className="projectTitle">Loopy</p>
         <div className="projectLinks">
           <a
@@ -17,54 +32,7 @@ const Projects = () => {
           </a>
         </div>
 
-        <p className="projectDescription">
-          Share and discover music you love
-        </p>
-      </div>
-      <div className="project">
-        <p className="projectTitle">Blossom</p>
-        <div className="projectLinks">
-          <a
-            href="https://blossomsurveys.io/"
-            target="_blank"
-            rel="noreferrer"
-            className="projectLink"
-          >
-            website
-          </a>
-        </div>
-
-        <p className="projectDescription">
-          Create beautiful surveys that help you understand your audience.
-        </p>
-      </div>
-      <div className="project">
-        <p className="projectTitle">Corked Up</p>
-        <div className="projectLinks">
-          <a
-            href="https://www.corkedup.wine/"
-            target="_blank"
-            rel="noreferrer"
-            className="projectLink"
-          >
-            website
-          </a>
-          <div>|</div>
-          <a
-            href="https://github.com/kylelong/corkedup"
-            target="_blank"
-            rel="noreferrer"
-            className="projectLinkTwo"
-          >
-            github
-          </a>
-        </div>
-
-        <p className="projectDescription">
-          Catering to wine lovers: find local events, daily flash deals on nice
-          bottles, and recipes for pairiing. All to enhance your wine drinking
-          experience.
-        </p>
+        <p className="projectDescription">Share and discover music you love</p>
       </div>
     </div>
   );
